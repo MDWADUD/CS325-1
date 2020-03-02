3db5f3415be111fe1af8e68587b2abb30e48e6cc
ReidConsole <jermainereid@mec.science>
2020-03-02 17:23:33 -0500

    Labs Mod

---
+++ Labs/lab04/lab04.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+double ArithmeticMean(ifstream& in)
+{
+    //Modify the body of this function
+    return 0.0;
+}
+
+double HarmonicMean(ifstream& in)
+{
+    //Modify the body of this function
+    return 0.0;
+}
+
+int main()
+{
+    /*Use the file numbers.txt to test your code*/
+    return 0;
+}
